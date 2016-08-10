0b5dcf675b52baae6a7ca3ab253e677e9ed3b9c4
mygreymatter <mygreymatter@gmail.com>
2016-08-10 10:13:44 +0530

    Implemented SharedElement transition

---
+++ app/src/main/java/com/mayo/animations/sharedelement/SharedElementActivity.java
@@ -0,0 +1,15 @@
+package com.mayo.animations.sharedelement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mayo.animations.R;
+
+public class SharedElementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared_element);
+    }
+}
